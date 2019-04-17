5aff9a100b78c47bed725739a55dc3e2cc016c74
jason <jiang911113@163.com>
2019-04-17 03:54:52 -0400

    phpgraphy

---
+++ README.md
@@ -0,0 +1,1 @@
+# phpGraphy-jasonsky
---
+++ conf/config.ini.php
@@ -0,0 +1,29 @@
+; <?php die() ?>
+; phpGraphy configuration script
+;
+; This is where you should configure all your settings, it can be done either
+; manually but it's highly recommended to use the admin web interface 
+; if you're not an experienced user.
+;
+; For the list of possibles directives and associated values, please read
+; the manual available either under the docs/ subdirectory or online from
+; phpGraphy website (http://phpgraphy.sourceforge.net)
+;
+; Any non-alphanumeric characters value needs to be enclosed in double-quotes (")
+
+admin_ip = "124.65.116.170"
+install_mode = 1
+debug_mode = 1
+language_file = "lang_zh.inc.php"
+thumb_generator = "convert"
+use_session = 0
+highres_min_level = 100
+lr_limit = 52428800
+thumb_quality = 100
+lr_quality = 100
+thumbs_order = "L2R"
+files_sort_by = "datetime"
+movie_extractor = "ffmpeg"
+dirs_sort_by = "datetime"
+directory_display_mode = "icon"
+use_rating = 0
